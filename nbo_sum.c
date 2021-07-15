c653e8ec134d4795dbb467af918df952e6c7db8f
p1nkjelly <p1nkjelly@pinkjelly.cat>
2021-07-15 21:18:57 +0900

    Add sum function.

---
+++ nbo_sum.c
@@ -0,0 +1,6 @@
+// nbo_sum.c
+#include "nbo_sum.h"
+
+uint32_t nbo_sum(uint32_t f1, uint32_t f2) {
+    return (f1 + f2);
+}
---
+++ nbo_sum.h
@@ -0,0 +1,5 @@
+// nbo_sum.h
+#pragma once
+#include <stdint.h>
+
+uint32_t nbo_sum(uint32_t f1, uint32_t f2);
